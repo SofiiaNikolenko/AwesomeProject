0542cb351a731ee6483094f65776405e8ab90904
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-07-04 21:53:12 +0200

    add files

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import RegistrationScreen from "./src/components/RegistrationScreen";
+import LoginScreen from "./src/components/LoginScreen";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./src/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./src/fonts/Roboto-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
     </View>
   );
 }
@@ -13,8 +23,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,161 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Button,
+} from "react-native";
+import PhotoBG from "../picture/PhotoBG.png";
+
+export default LoginScreen = () => (
+  <View style={styles.container}>
+    <ImageBackground source={PhotoBG} style={styles.image}>
+      <View style={styles.overlay}>
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>Увійти</Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Адреса електронної пошти"
+            placeholderTextColor="rgba(189, 189, 189, 1)"
+          ></TextInput>
+          <View style={styles.viewLastImput}>
+            <TextInput
+              style={[styles.input, styles.inputLastMargin]}
+              placeholder="Пароль"
+              placeholderTextColor="rgba(189, 189, 189, 1)"
+            ></TextInput>
+            <TouchableOpacity style={styles.viewLastImputButton}>
+              <Text style={styles.viewLastImputText}>Показати</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style={styles.enterButton}>
+            <Text style={styles.enterButtonText}>Увійти</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.linkLogIn}>
+            <Text style={styles.textSignUp}>
+              Немає акаунту?
+              <Text style={[styles.textSignUp, styles.textSignUpUnderlin]}>
+                Зареєструватися
+              </Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  image: {
+    flex: 1,
+  },
+
+  overlay: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+
+  contentContainer: {
+    position: "relative",
+    backgroundColor: "#fff",
+    width: "100%",
+    height: 489,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    alignItems: "center",
+  },
+
+  title: {
+    fontFamily: "Roboto-Medium",
+    fontSize: 30,
+    fontWeight: "500",
+    lineHeight: 35,
+    marginTop: 32,
+    marginBottom: 33,
+  },
+
+  input: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 19,
+    height: 50,
+    width: 343,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "rgba(232, 232, 232, 1)",
+    backgroundColor: "rgba(246, 246, 246, 1)",
+    paddingLeft: 16,
+    marginBottom: 16,
+  },
+
+  inputLastMargin: {
+    marginBottom: 43,
+  },
+
+  visibleButton: {
+    height: 5,
+    width: 20,
+  },
+
+  viewLastImput: {
+    position: "relative",
+  },
+
+  viewLastImputButton: {
+    position: "absolute",
+    top: 16,
+    right: 16,
+  },
+
+  viewLastImputText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 19,
+    color: "rgba(27, 67, 113, 1)",
+  },
+
+  enterButton: {
+    backgroundColor: "rgba(255, 108, 0, 1)",
+    borderRadius: 100,
+    marginBottom: 16,
+  },
+
+  enterButtonText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 19,
+    color: "rgba(255, 255, 255, 1)",
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingRight: 111,
+    paddingLeft: 111,
+  },
+
+  linkLogIn: {},
+
+  textSignUp: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 19,
+    color: "rgba(27, 67, 113, 1)",
+  },
+
+  textSignUpUnderlin: {
+    textDecorationLine: "underline",
+  },
+});
