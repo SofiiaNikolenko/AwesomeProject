eeb300314df21878505dc6ab60a8ce26826af9a8
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-07-06 23:29:03 +0200

    add small changes

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
-import RegistrationScreen from "./src/components/RegistrationScreen";
-import LoginScreen from "./src/components/LoginScreen";
+import { StyleSheet, View, ImageBackground } from "react-native";
+import RegistrationScreen from "./src/screens/RegistrationScreen";
+import LoginScreen from "./src/screens/LoginScreen";
 import { useFonts } from "expo-font";
+import PhotoBG from "./src/picture/PhotoBG.png";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,10 +13,17 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
+
   return (
     <View style={styles.container}>
-      <RegistrationScreen />
-      {/* <LoginScreen /> */}
+      <ImageBackground source={PhotoBG} style={styles.image}>
+        <View style={styles.overlay}>
+          <View style={styles.contentContainer}>
+            <RegistrationScreen />
+            {/* <LoginScreen /> */}
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -23,6 +31,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  image: {
+    flex: 1,
+  },
+
+  overlay: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+
+  contentContainer: {
+    position: "relative",
     backgroundColor: "#fff",
+    width: "100%",
+    height: 549,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    alignItems: "center",
   },
 });
